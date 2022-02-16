573ef9e6ddf5419c8507a697e700408b8f71e093
Csaba Kiss <csaba.kiss@tipico.com>
2022-02-16 18:14:01 +0100

    disable logging compose

--- request/request.go
+++ request/request.go
@@ -1,7 +1,6 @@
 package request
 
 import (
-	"log"
 	"net/http"
 
 	execconf "github.com/csabakissmalta/tpee/exec"
@@ -11,20 +10,20 @@ import (
 // Mostly the executable request's lifecycle-related operations
 
 func ComposeHttpRequest(r *postman.Request, env []*execconf.ExecEnvironmentElem) *http.Request {
-	log.Println(r.Method)
-	log.Println(r.Auth)
-	if len(r.Body.Urlencoded) > 0 {
-		for _, b := range r.Body.Urlencoded {
-			log.Println(b.Value)
-		}
-	}
-	if len(r.Body.Formdata) > 0 {
-		for _, f := range r.Body.Formdata {
-			log.Println(f.Value)
-		}
-	}
-	log.Println(r.Header)
-	log.Println(r.URL)
+	// log.Println(r.Method)
+	// log.Println(r.Auth)
+	// if len(r.Body.Urlencoded) > 0 {
+	// 	for _, b := range r.Body.Urlencoded {
+	// 		log.Println(b.Value)
+	// 	}
+	// }
+	// if len(r.Body.Formdata) > 0 {
+	// 	for _, f := range r.Body.Formdata {
+	// 		log.Println(f.Value)
+	// 	}
+	// }
+	// log.Println(r.Header)
+	// log.Println(r.URL)
 
 	// r_res := &http.Request{
 	// 	Method: r.Method,
