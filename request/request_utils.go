619c62039a80648cbe7db9a5299cfc3fd6d18945
Csaba Kiss <csaba.kiss@tipico.com>
2022-02-17 12:43:57 +0100

    regexp matching for replacement #2

--- request/request_utils.go
+++ request/request_utils.go
@@ -1,35 +1,39 @@
 package request
 
 import (
-	"log"
+	"fmt"
 	"regexp"
+	"strings"
 
 	timeline "github.com/csabakissmalta/tpee/timeline"
 )
 
 func validate_and_substitute_feed_type(in *string, r_var *regexp.Regexp, fds []*timeline.Feed) (*string, error) {
-	// match := r_var.FindStringSubmatch(*in)
-	// if len(match) > 1 {
-	// 	varname := match[1]
-	// 	var ch chan interface{}
-	// 	for _, feed := range fds {
-	// 		if varname == feed.Name {
-	// 			ch = feed.Value
-	// 			break
-	// 		}
-	// 	}
-	// 	if ch != nil {
-	// 		repl := <-ch
-	// 		*in = strings.Replace(*in, match[1], repl.(string), -1)
-	// 	} else {
-	// 		e := fmt.Errorf("the variable match has failed, no feed value is substituted")
-	// 		return in, e
-	// 	}
-	// }
 	match := r.FindStringSubmatch(*in)
+	var feed_varname string
+	var ch chan interface{}
+	var env_var_to_replace string
+	var env_var_replace_string interface{}
 	for i, name := range r.SubexpNames() {
 		if i > 0 && i <= len(match) {
-			log.Printf("MATHED: var: %s :: value: %s", name, match[i])
+			// log.Printf("MATCHED: var: %s :: value: %s", name, match[i])
+			if name == "FEED_VAR" {
+				feed_varname = match[i]
+				for _, feed := range fds {
+					if feed_varname == feed.Name {
+						ch = feed.Value
+					}
+				}
+				if ch != nil {
+					env_var_replace_string = <-ch
+				} else {
+					e := fmt.Errorf("the variable match has failed, no feed value is substituted")
+					return in, e
+				}
+			} else if name == "WHOLE" {
+				env_var_to_replace = match[i]
+				*in = strings.Replace(*in, env_var_to_replace, env_var_replace_string.(string), -1)
+			}
 		}
 	}
 	return in, nil
