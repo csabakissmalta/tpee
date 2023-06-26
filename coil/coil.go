2fbc5272923fef3cba741cd3e38386dd490cdba3
Csaba Kiss <csaba.kiss@tipico.com>
2023-06-26 17:59:07 +0200

    compare exectimes with real exec time for rampup tasks #23

--- coil/coil.go
+++ coil/coil.go
@@ -210,7 +210,7 @@ func (c *Coil) consumeTimelineTimerMode(tl *timeline.Timeline, env []*execconf.E
 			case next = <-tl.RampupTasks:
 				// if there is rampup, it falls back to compare mode
 				elapsedTotal := time.Since(testStartTime).Nanoseconds()
-				corr = elapsedTotal - int64(next.PlannedExecTimeNanos)
+				corr = elapsedTotal - time.Duration(next.PlannedExecTimeNanos).Nanoseconds()
 				planned_delta := next.PlannedExecTimeNanos - tl.CurrectTask.PlannedExecTimeNanos
 				dorm_period := planned_delta - int(corr)
 
