995848e09d9ede4e8e83a25c0eedf963fd16cf40
Csaba Kiss <csaba.kiss@tipico.com>
2024-01-05 18:33:10 +0100

    NATS input implemented #15

--- timeline/timeline_utils.go
+++ timeline/timeline_utils.go
@@ -129,7 +129,8 @@ func load_feeds_if_required(dim int, env []*execconf.ExecEnvironmentElem, subs m
 			if fd == nil {
 				// that means, it is a NATS subscription
 				ch_item := envElem.Key
-				log.Println("::: ENV ELEM KEY ::: ", ch_item, subs[ch_item])
+				tester := <-subs[ch_item]
+				log.Println("::: ENV ELEM KEY ::: ", ch_item, tester.Subject)
 				fd = &Feed{
 					Name:      ch_item,
 					NATSValue: subs[ch_item],
